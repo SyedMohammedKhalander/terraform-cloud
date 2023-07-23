266a4f7c7f05cfebac6eb915638e73adbf083880
Syed_M <42533818+SyedMohammedKhalander@users.noreply.github.com>
2023-07-23 23:20:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     =  "us-west-1"
+}
+
+
 resource "aws_instance" "terraform_instance" {
   ami           = "ami-057752b3f1d6c4d6c"
   instance_type = "t2.micro"
@@ -5,7 +10,4 @@ resource "aws_instance" "terraform_instance" {
   tags = {
     Name = "HelloWorld"
   }
-}
-provider "aws" {
-  region     =  "us-west-1"
 }
