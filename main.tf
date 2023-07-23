c635549a031dac9eec76da52fd99b3ef1ce678dd
Syed_M <42533818+SyedMohammedKhalander@users.noreply.github.com>
2023-07-23 23:12:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "terraform_instance" {
+  ami           = "ami-057752b3f1d6c4d6c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+provider "aws" {
+  region     =  "us-west-1"
+}
